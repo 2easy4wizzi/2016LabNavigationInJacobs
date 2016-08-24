5f44bc5586ace302607b702f1989dcf7c7aedfde
gilad eini <giladeini@gmail.com>
2016-08-24 20:22:48 +0300

    reading result of getShortestPath is working.
    added a viewby combo. we will use it to display rooms by names (shuli's
    office) or by numbers (313)

--- engine/graph.h
+++ engine/graph.h
@@ -8,32 +8,35 @@
 #define INFVALUE 1000000
 #define qPair pair< Node*, int >
 
+//comperator
 struct comp {
 	bool operator() (const qPair &a, const qPair &b) {
 		return a.second > b.second;
 	}
 };
-struct pathRoom { Node* room; int distance; string direction; };
+
+//return nodes from GetShortestpath() in this struct
+struct pathRoom {
+    Node* room;
+    int distance;
+    string direction;
+    int nextRoomInPathId;
+};
 
 class Graph
 {
+public:
+    Graph(string xmlPathNodes, string xmlPathEdges, bool& succesReadingXmls);
+    ~Graph();
+    bool ParseXmlNodes(string xmlPathNodes);
+    bool ParseXmlEdges(string xmlPathEdges);
+    list<Node*> GetGrapghNodes() const;
+    list<Edge*> GetGrapghEdges() const;
+    list<pathRoom> GetShortestpath(Node* start, Node* end);
+
 private:
-	priority_queue< qPair, vector< qPair >, comp > Queue;
-	vector< qPair > Adjacency[MAXNODES];
-	int distanceV[MAXNODES];
-	bool wasChecked[MAXNODES];
 	list<Node*>* _nodes;
 	list<Edge*>* _edges;
-	list<pathRoom>* _shortestPath;
-public:
-    Graph(string xmlPathNodes,string xmlPathEdges);
-    ~Graph();
-    void ParseXmlNodes(string xmlPathNodes);
-    void ParseXmlEdges(string xmlPathEdges);
-	list<Node*> GetGrapghNodes() const;
-	list<Edge*> GetGrapghEdges() const;
-	Node* GetNodeByName(string nodeName);
-	list<pathRoom> GetShortestpath(Node* start, Node* end);
 };
 
 #endif // GRAPH_H
